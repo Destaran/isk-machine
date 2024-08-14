3710b044ad47cc98d4bd11be83235e1673e52ce3
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-08-14 15:08:32 +0200

    chore: remove old api utils

--- src/API/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({ baseURL: "https://esi.evetech.net/latest/" });
-
-export async function get<T>(path: string) {
-  const { data } = await api.get<T>(path);
-  return data;
-}
