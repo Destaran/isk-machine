35eaa02e4318644890b21fb518cd21734489212c
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-08-20 20:00:01 +0200

    fix: useMarketGroup return data

--- src/hooks/markets/useMarketGroup.ts
+++ src/hooks/markets/useMarketGroup.ts
@@ -11,5 +11,6 @@ export function useMarketGroup(id: number) {
   return useQuery({
     queryKey: ["marketGroup", id],
     queryFn: () => getMarketsGroupsMarketGroupId(options),
+    select: (response) => response.data,
   });
 }
