1149474cad3524cb81eaf0548e6f36be704ac996
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-09-25 14:18:13 +0200

    feat: implement redux store with marketSlice

---
+++ src/redux/orders/ordersSlice.ts
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '../store';
+import {
+  GetMarketsRegionIdOrdersResponse,
+  GetUniverseTypesTypeIdResponse,
+} from '../../hey-api';
+
+interface MarketState {
+  type: null | GetUniverseTypesTypeIdResponse;
+  region: number;
+  location: null | number;
+  orders: GetMarketsRegionIdOrdersResponse;
+}
+
+const initialState: MarketState = {
+  type: null,
+  region: 10000002,
+  location: null,
+  orders: [],
+};
+
+export const marketSlice = createSlice({
+  name: 'market',
+  initialState,
+  reducers: {
+    setOrders: (
+      state,
+      action: PayloadAction<GetMarketsRegionIdOrdersResponse>
+    ) => {
+      state.orders = action.payload;
+    },
+    resetOrders: (state) => {
+      state.orders = [];
+    },
+  },
+});
+
+export const { setOrders, resetOrders } = marketSlice.actions;
+
+export const selectOrders = (state: RootState) => {
+  const orders = state.market.orders;
+  const buy = orders
+    .filter((order) => order.is_buy_order)
+    .sort((a, b) => b.price - a.price);
+
+  const sell = orders
+    .filter((order) => !order.is_buy_order)
+    .sort((a, b) => a.price - b.price);
+
+  return { buy, sell };
+};
+
+export default marketSlice.reducer;
