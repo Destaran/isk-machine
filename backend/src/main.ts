083215d270364dc8f8abe50da664d76db46f5bb2
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-09-30 01:28:37 +0200

    feat: improve cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:5173', // Replace with the origin of your frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // If your frontend includes credentials (like cookies)
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
