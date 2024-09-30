8a87fd289c752ec2a1de5b85710a7dcb696c63f3
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-09-30 02:03:18 +0200

    feat: prep market.service for future use

--- backend/src/market/market.module.ts
+++ backend/src/market/market.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { MarketService } from './market.service';
 import { MarketController } from './market.controller';
 import { OrderRepository } from 'src/data-scraper/order.repository';
+import { DataScraperService } from 'src/data-scraper/data-scraper.service';
+import { HttpModule } from '@nestjs/axios';
+import { RegionRepository } from 'src/data-scraper/region.repository';
 
 @Module({
-  providers: [MarketService, OrderRepository],
+  imports: [HttpModule],
+  providers: [
+    MarketService,
+    OrderRepository,
+    DataScraperService,
+    RegionRepository,
+  ],
   controllers: [MarketController],
 })
 export class MarketModule {}
--- backend/src/market/market.service.ts
+++ backend/src/market/market.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { DataScraperService } from 'src/data-scraper/data-scraper.service';
 import { OrderRepository } from 'src/data-scraper/order.repository';
 
 @Injectable()
 export class MarketService {
-  constructor(private readonly orderRepository: OrderRepository) {}
+  constructor(
+    private readonly orderRepository: OrderRepository,
+    private readonly dataScraperService: DataScraperService,
+  ) {}
 
   async getOrdersByTypeId(typeId: number) {
     const orders = await this.orderRepository.find({
       where: { type_id: typeId },
     });
+
+    // const uniquieRegionIds = [
+    //   ...new Set(orders.map((order) => order.region_id)),
+    // ];
+    // const regions = await this.dataScraperService.postNames(uniquieRegionIds);
+
     return orders;
   }
 }
