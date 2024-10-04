f0367c8b0a695d9392af7632670b798229bedd31
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-04 20:15:58 +0200

    feat: add TextFilters to Filters

--- site/src/Market/Filters.tsx
+++ site/src/Market/Filters.tsx
@@ -1,11 +1,22 @@
 import styled from 'styled-components';
 import { Filter } from './Filter';
+import { TextFilter } from './TextFilter';
 
 const Container = styled.div``;
 
 export function Filters() {
   return (
     <Container>
+      <TextFilter
+        name="Location"
+        type={'locationFilter'}
+        defaultValue="Jita IV - Moon 4"
+      />
+      <TextFilter
+        name="Region"
+        type={'regionFilter'}
+        defaultValue="The Forge"
+      />
       <Filter name="Market Hubs only" type={'marketHubsFilter'} />
     </Container>
   );
