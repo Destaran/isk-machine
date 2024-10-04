fc4423dd4e7db96ed981923ed66836dbdf5c9ec8
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-05 01:29:53 +0200

    feat: useSearch hook

---
+++ site/src/api/market/useSearch.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import { get } from '../API';
+import { useCallback } from 'react';
+import { SearchResult } from './SearchResult';
+
+async function getTypes(searchTerm: string) {
+  console.log(searchTerm);
+
+  return await get<SearchResult[]>(`/market/search?s=${searchTerm}`);
+}
+
+export function useSearch(searchTerm: string, enabled: boolean) {
+  const getCurrentOrders = useCallback(
+    async () => await getTypes(searchTerm),
+    [searchTerm]
+  );
+  return useQuery({
+    queryKey: ['search', searchTerm],
+    queryFn: getCurrentOrders,
+    enabled,
+  });
+}
