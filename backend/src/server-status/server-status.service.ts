42af57a0a4fa5032b275960a731a85592073e94a
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-05 17:01:38 +0200

    feat: improve server-status naming

--- backend/src/server-status/server-status.service.ts
+++ backend/src/server-status/server-status.service.ts
@@ -4,7 +4,7 @@ import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class ServerStatusService {
-  private currentPlayers: number | null = null;
+  private players: number | null = null;
   private serverVersion: string | null = null;
   private startTime: string | null = null;
   private fetchInterval: NodeJS.Timeout;
@@ -32,7 +32,7 @@ export class ServerStatusService {
     const response = await firstValueFrom(this.httpService.get(url));
 
     const status = response.data;
-    this.currentPlayers = status.players;
+    this.players = status.players;
     this.serverVersion = status.server_version;
     this.startTime = status.start_time;
     console.log('Fetched server status.');
@@ -40,9 +40,9 @@ export class ServerStatusService {
 
   async getStatus() {
     return {
-      players: this.currentPlayers,
-      server_version: this.serverVersion,
-      start_time: this.startTime,
+      players: this.players,
+      serverVersion: this.serverVersion,
+      startTime: this.startTime,
     };
   }
 }
