16506996af6d226a78a065d46a9f880f9aa61494
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-05 18:39:16 +0200

    feat: add ExcludeFIlter

---
+++ site/src/Market/Navigation/ExcludeFIlter.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { Filter } from './Filter';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Wrapper = styled.div``;
+
+const Text = styled.p`
+  color: white;
+  margin-right: 20px;
+`;
+
+export function ExcludeFilter() {
+  return (
+    <Container>
+      <Text>Exclude</Text>
+      <Wrapper>
+        <Filter name="Null Sec" type={'excludeNullSecFilter'} color="red" />
+        <Filter name="Low Sec" type={'excludeLowSecFilter'} color="orange" />
+        <Filter name="High Sec" type={'excludeHighSecFilter'} color="cyan" />
+        <Filter name="NPC Stations" type={'excludeStationsFilter'} />
+        <Filter name="Player Structures" type={'excludeStructuresFilter'} />
+      </Wrapper>
+    </Container>
+  );
+}
