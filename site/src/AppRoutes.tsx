859887c511a99caf1e51fbb65da6bb2e16720a42
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-05 15:58:48 +0200

    feat: add Landing placeholder

--- site/src/AppRoutes.tsx
+++ site/src/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { Market } from './Market/Market';
 import { Navigation } from './Navigation';
+import { Landing } from './Landing/Landing';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index element={<Market />} />
+        <Route index element={<Landing />} />
         <Route path="market" element={<Market />} />
         <Route path="market/:id" element={<Market />} />
       </Route>
---
+++ site/src/Landing/Landing.tsx
@@ -0,0 +1,9 @@
+import { PageBase } from '../components/PageBase';
+
+export function Landing() {
+  return (
+    <PageBase>
+      <h1>Welcome to the Landing Page</h1>
+    </PageBase>
+  );
+}
