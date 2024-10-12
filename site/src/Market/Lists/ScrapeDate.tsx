2976f8ba24670581b90bbf9ff5971a65a965d98e
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-12 18:23:25 +0200

    feat: add ScrapeDate component

---
+++ site/src/Market/Lists/ScrapeDate.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const Text = styled.p`
+  color: white;
+`;
+
+interface Props {
+  timestamp: number;
+}
+
+export function ScrapeDate({ timestamp }: Props) {
+  const updateDate = new Date(timestamp);
+  const nowDate = new Date();
+  const diff = nowDate.getTime() - updateDate.getTime();
+  const diffHours = Math.floor(diff / 1000 / 60 / 60);
+  const diffMinutes = Math.floor((diff / 1000 / 60) % 60);
+  const timePassed = diffHours > 0 ? `${diffHours} hours` : `${diffMinutes} minutes`;
+  return (
+    <Container>
+      <Text>Last updated {timePassed} ago.</Text>
+    </Container>
+  );
+}
