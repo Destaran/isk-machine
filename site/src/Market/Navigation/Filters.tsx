5b39e70fd47bea4d17823546b8b677c34e55b017
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-14 00:26:49 +0200

    feat: use NewFilter

--- site/src/Market/Navigation/Filters.tsx
+++ site/src/Market/Navigation/Filters.tsx
@@ -1,20 +1,52 @@
-import { Filter } from './Filter';
 import { TextFilter } from './TextFilter';
-import { ExcludeFilters } from './ExcludeFIlters';
 import { SectionTitle } from './SectionTitle';
+import { useAppSelector } from '../../redux/hooks';
+import { filterStates } from '../../redux/orders/ordersSlice';
+import { NewFilter } from './NewFilter';
 
 export function Filters() {
+  const states = useAppSelector(filterStates);
+
   return (
     <div>
       <SectionTitle>Filters</SectionTitle>
-      <ExcludeFilters />
+      <NewFilter
+        name="Null Sec"
+        type="excludeNullSecFilter"
+        state={states.excludeNullSecFilter}
+        exclude
+      />
+      <NewFilter
+        name="Low Sec"
+        type="excludeLowSecFilter"
+        state={states.excludeLowSecFilter}
+        exclude
+      />
+      <NewFilter
+        name="High Sec"
+        type="excludeHighSecFilter"
+        state={states.excludeHighSecFilter}
+        exclude
+      />
+      <NewFilter
+        name="Stations"
+        type="excludeStationsFilter"
+        state={states.excludeStationsFilter}
+        exclude
+      />
+      <NewFilter
+        name="Structures"
+        type="excludeStructuresFilter"
+        state={states.excludeStructuresFilter}
+        exclude
+      />
       <TextFilter
         name="Location"
-        type={'locationFilter'}
+        type="locationFilter"
         defaultValue="Jita IV - Moon 4 - Caldari Navy"
       />
       <TextFilter name="Region" type={'regionFilter'} defaultValue="The Forge" />
-      <Filter name="Market Hubs only" type={'marketHubsFilter'} />
+      <NewFilter name="Market Hubs" type={'marketHubsFilter'} state={states.marketHubsFilter} />
     </div>
   );
 }
