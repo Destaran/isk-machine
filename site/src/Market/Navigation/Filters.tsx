69012a32cc5824d55cd45a5c5d4c6fe2e99903d0
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-14 00:52:18 +0200

    feat: use NewTextFilter and style Filters

--- site/src/Market/Navigation/Filters.tsx
+++ site/src/Market/Navigation/Filters.tsx
@@ -1,8 +1,22 @@
-import { TextFilter } from './TextFilter';
 import { SectionTitle } from './SectionTitle';
 import { useAppSelector } from '../../redux/hooks';
 import { filterStates } from '../../redux/orders/ordersSlice';
 import { NewFilter } from './NewFilter';
+import { NewTextFilter } from './NewTextFilter';
+import styled from 'styled-components';
+
+const Separator = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.emDarkGrey};
+  margin: 10px auto;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
 
 export function Filters() {
   const states = useAppSelector(filterStates);
@@ -10,43 +24,56 @@ export function Filters() {
   return (
     <div>
       <SectionTitle>Filters</SectionTitle>
-      <NewFilter
-        name="Null Sec"
-        type="excludeNullSecFilter"
-        state={states.excludeNullSecFilter}
-        exclude
-      />
-      <NewFilter
-        name="Low Sec"
-        type="excludeLowSecFilter"
-        state={states.excludeLowSecFilter}
-        exclude
-      />
-      <NewFilter
-        name="High Sec"
-        type="excludeHighSecFilter"
-        state={states.excludeHighSecFilter}
-        exclude
-      />
-      <NewFilter
-        name="Stations"
-        type="excludeStationsFilter"
-        state={states.excludeStationsFilter}
-        exclude
-      />
-      <NewFilter
-        name="Structures"
-        type="excludeStructuresFilter"
-        state={states.excludeStructuresFilter}
-        exclude
-      />
-      <TextFilter
+      <Wrapper>
+        <NewFilter
+          name="Null Sec"
+          type="excludeNullSecFilter"
+          state={states.excludeNullSecFilter}
+          exclude
+        />
+        <NewFilter
+          name="Low Sec"
+          type="excludeLowSecFilter"
+          state={states.excludeLowSecFilter}
+          exclude
+        />
+        <NewFilter
+          name="High Sec"
+          type="excludeHighSecFilter"
+          state={states.excludeHighSecFilter}
+          exclude
+        />
+      </Wrapper>
+      <Separator />
+      <Wrapper>
+        <NewFilter
+          name="Stations"
+          type="excludeStationsFilter"
+          state={states.excludeStationsFilter}
+          exclude
+        />
+        <NewFilter
+          name="Structures"
+          type="excludeStructuresFilter"
+          state={states.excludeStructuresFilter}
+          exclude
+        />
+      </Wrapper>
+      <Separator />
+
+      <NewTextFilter
         name="Location"
         type="locationFilter"
+        state={states.locationFilter}
         defaultValue="Jita IV - Moon 4 - Caldari Navy"
       />
-      <TextFilter name="Region" type={'regionFilter'} defaultValue="The Forge" />
-      <NewFilter name="Market Hubs" type={'marketHubsFilter'} state={states.marketHubsFilter} />
+      <NewTextFilter
+        name="Region"
+        type={'regionFilter'}
+        state={states.regionFilter}
+        defaultValue="The Forge"
+      />
+      <NewFilter name="Hubs" type={'marketHubsFilter'} state={states.marketHubsFilter} />
     </div>
   );
 }
