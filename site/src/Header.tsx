11722a0c948e4a035557a1c5d1e8716866e9f1bb
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-13 04:26:43 +0200

    style: Header anim

--- site/src/Header.tsx
+++ site/src/Header.tsx
@@ -1,8 +1,21 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { ServerStatus } from './ServerStatus';
+import { theme } from './style/theme';
+
+const BgAnim = keyframes`
+from {
+  background-position: left;
+} 
+to {
+  background-position: right;
+}
+`;
 
 const Container = styled.div`
-  background-color: ${({ theme }) => theme.colors.emGrey};
+  /* background-color: ${({ theme }) => theme.colors.emGrey}; */
+  background-image: linear-gradient(315deg, #4d4d4d 45%, ${theme.colors.orange} 50%, #4d4d4d 55%);
+  background-size: 1800%;
+  animation: ${BgAnim} 30s infinite;
   padding: 10px;
   margin: 10px;
   border-radius: 5px;
