a4f665212b24f574ff7557d7b2ddfc3672527649
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-14 00:47:35 +0200

    feat: add NewTextFilter

---
+++ site/src/Market/Navigation/NewTextFilter.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { useAppDispatch } from '../../redux/hooks';
+import { setFilter, TextFilterKey } from '../../redux/orders/ordersSlice';
+import { NewFilter } from './NewFilter';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TextInput = styled.input`
+  margin: 0;
+  padding: 0;
+  height: 25px;
+  width: 60%;
+  background-color: ${({ theme }) => theme.colors.emWhite};
+`;
+
+interface Props {
+  name: string;
+  type: TextFilterKey;
+  state: boolean;
+  exclude?: boolean;
+  defaultValue?: string;
+}
+
+export function NewTextFilter({ name, type, state, exclude, defaultValue }: Props) {
+  const dispatch = useAppDispatch();
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    dispatch(setFilter({ filter: e.target.value, type }));
+  }
+
+  return (
+    <Container>
+      <NewFilter name={name} type={type} state={state} exclude={exclude} />
+      <TextInput type="text" onChange={handleInput} defaultValue={defaultValue ?? ''} />
+    </Container>
+  );
+}
