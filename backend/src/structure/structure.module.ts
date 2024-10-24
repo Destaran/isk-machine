7ba024b3c90762ad0ce730399965ebfd165cc2d0
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-24 23:52:51 +0200

    chore: structure module

--- backend/src/structure/structure.module.ts
+++ backend/src/structure/structure.module.ts
@@ -4,9 +4,11 @@ import { HttpModule } from '@nestjs/axios';
 import { Structure } from './structure.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SystemModule } from 'src/system/system.module';
+import { StructureRepository } from './structure.repository';
 
 @Module({
   imports: [HttpModule, TypeOrmModule.forFeature([Structure]), SystemModule],
-  providers: [StructureService],
+  providers: [StructureService, StructureRepository],
+  exports: [StructureService],
 })
 export class StructureModule {}
