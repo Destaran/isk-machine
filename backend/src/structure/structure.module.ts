8811316941718ec55e844b0c56b5a86fc85eb49a
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-10-24 23:13:28 +0200

    feat: update structure.module

--- backend/src/structure/structure.module.ts
+++ backend/src/structure/structure.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StructureService } from './structure.service';
+import { HttpModule } from '@nestjs/axios';
+import { Structure } from './structure.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SystemModule } from 'src/system/system.module';
 
 @Module({
-  providers: [StructureService]
+  imports: [HttpModule, TypeOrmModule.forFeature([Structure]), SystemModule],
+  providers: [StructureService],
 })
 export class StructureModule {}
