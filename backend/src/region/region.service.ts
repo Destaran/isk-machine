35b3542572e505903ee1ca0fec41ce0acf39e354
Balázs Abonyi <abonyibalazs95@gmail.com>
2025-03-22 16:59:04 +0100

    refactor: region.service

--- backend/src/region/region.service.ts
+++ backend/src/region/region.service.ts
@@ -7,50 +7,80 @@ import { In } from 'typeorm';
 
 @Injectable()
 export class RegionService {
+  private failedRegionIds: object = {};
   constructor(
     private readonly httpService: HttpService,
     private readonly regionRepository: RegionRepository,
   ) {}
 
-  async wipe() {
-    await this.regionRepository.clear();
-  }
-
-  async scrape() {
-    this.wipe();
+  async fetchRegionIds(): Promise<number[]> {
     const regionIdsUrl =
       'https://esi.evetech.net/latest/universe/regions/?datasource=tranquility';
 
-    const regionIdsRequest = await firstValueFrom(
-      this.httpService.get(regionIdsUrl),
-    );
-
-    const regionIds = regionIdsRequest.data;
-
-    if (regionIdsRequest.status === 200) {
-      console.log(`Scraped ${regionIds.length} regions.`);
+    try {
+      const regionIdsRequest = await firstValueFrom(
+        this.httpService.get(regionIdsUrl),
+      );
+      return regionIdsRequest.data;
+    } catch (error) {
+      console.error('Failed to fetch region IDs:', error.message);
+      return [];
     }
+  }
 
-    for (const regionId of regionIds) {
-      const regionUrl = `https://esi.evetech.net/latest/universe/regions/${regionId}/?datasource=tranquility`;
+  async fetchRegion(regionId: number): Promise<any> | null {
+    const regionUrl = `https://esi.evetech.net/latest/universe/regions/${regionId}/?datasource=tranquility`;
 
+    try {
       const regionRequest = await firstValueFrom(
         this.httpService.get(regionUrl),
       );
-
-      if (regionRequest.status === 200) {
-        console.log(`Scraped region ${regionId}.`);
-      }
-
-      const scrapedRegion = regionRequest.data;
-
-      const region = new Region();
-      region.id = scrapedRegion.region_id;
-      region.name = scrapedRegion.name;
-      region.description = scrapedRegion.description;
-      await this.regionRepository.save(region);
+      return regionRequest.data;
+    } catch (error) {
+      console.log(`Failed to fetch region ${regionId}`);
+      console.log(error.message);
+      this.failedRegionIds[regionId] = true;
+      return null;
     }
-    console.log(`Saved ${regionIds.length} regions.`);
+  }
+
+  async fetchRegions(regionIds: number[]): Promise<any[]> {
+    const regions = regionIds.map((regionId) => this.fetchRegion(regionId));
+    const fetchedRegions = await Promise.all(regions);
+    return fetchedRegions.filter((region) => region !== null);
+  }
+
+  async saveRegions(regions: any[]) {
+    for (const region of regions) {
+      const regionEntity = new Region();
+      regionEntity.id = region.region_id;
+      regionEntity.name = region.name;
+      regionEntity.description = region.description;
+
+      await this.regionRepository.save(regionEntity);
+
+      if (this.failedRegionIds[region.region_id]) {
+        delete this.failedRegionIds[region.region_id];
+      }
+    }
+  }
+
+  async scrape() {
+    await this.regionRepository.clear();
+    this.failedRegionIds = {};
+
+    const regionIds = await this.fetchRegionIds();
+    const regions = await this.fetchRegions(regionIds);
+    await this.saveRegions(regions);
+
+    const failedRegionIds = Object.keys(this.failedRegionIds).map(Number);
+    while (failedRegionIds.length > 0) {
+      const regions = await this.fetchRegions(failedRegionIds);
+      await this.saveRegions(regions);
+    }
+
+    const count = await this.regionRepository.count();
+    console.log(`Scraped ${count} regions`);
   }
 
   async getRegions() {
