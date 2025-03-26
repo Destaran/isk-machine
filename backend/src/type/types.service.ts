af37ca995ab043967de9e0c0be8ad2136f02e626
Balázs Abonyi <abonyibalazs95@gmail.com>
2025-03-26 13:56:14 +0100

    chore: remove unused code

--- backend/src/type/types.service.ts
+++ backend/src/type/types.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { TypesRepository } from './types.repository';
-import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
 import { Type } from './type.entity';
 import { DataScraper } from 'src/data-scraper/data-scraper';
 import { SmartUrl } from 'src/data-scraper/smart-url';
@@ -10,39 +8,11 @@ import { SmartUrl } from 'src/data-scraper/smart-url';
 export class TypesService {
   constructor(
     private readonly typeRepository: TypesRepository,
-    private readonly httpService: HttpService,
     private readonly dataScraper: DataScraper,
   ) {}
 
   async wipe() {
     await this.typeRepository.clear();
-  }
-
-  async scrapeTypeIdsByPage(pageNum: number) {
-    let typeIdsUrl = `https://esi.evetech.net/latest/universe/types/?datasource=tranquility&page=${pageNum}`;
-
-    const typeRequest = await firstValueFrom(
-      this.httpService.get(typeIdsUrl, {
-        validateStatus: (status) => {
-          return (
-            status === 200 || status === 404 || status === 504 || status === 500
-          );
-        },
-        timeout: 15000,
-      }),
-    );
-
-    typeIdsUrl = null;
-
-    return typeRequest;
-  }
-
-  async scrapeType(typeId: number) {
-    const typeUrl = `https://esi.evetech.net/latest/universe/types/${typeId}/?datasource=tranquility`;
-
-    const typeRequest = await firstValueFrom(this.httpService.get(typeUrl));
-
-    return typeRequest;
   }
 
   async scrape() {
