8837e996867436b0f79a6f0eb1381f9fb55853f8
Balázs Abonyi <abonyibalazs95@gmail.com>
2025-09-03 11:51:57 +0200

    chore: remove comments

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,10 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5173', // Replace with the origin of your frontend
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // If your frontend includes credentials (like cookies)
+    credentials: true,
   };
   app.enableCors(corsOptions);
   await app.listen(3000);
