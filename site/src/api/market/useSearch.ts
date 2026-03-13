8628a932c82337c7ce260f7d52ce52183c8d2fe0
Balázs Abonyi <abonyibalazs95@gmail.com>
2026-03-13 21:43:53 +0100

    chore: format

--- site/src/api/market/useSearch.ts
+++ site/src/api/market/useSearch.ts
@@ -1,7 +1,7 @@
-import { useQuery } from '@tanstack/react-query';
-import { get } from '../API';
-import { useCallback } from 'react';
-import { SearchResult } from './SearchResult';
+import { useQuery } from "@tanstack/react-query";
+import { get } from "../API";
+import { useCallback } from "react";
+import { SearchResult } from "./SearchResult";
 
 async function getTypes(searchTerm: string) {
   return await get<SearchResult[]>(`/market/search?s=${searchTerm}`);
@@ -10,10 +10,10 @@ async function getTypes(searchTerm: string) {
 export function useSearch(searchTerm: string, enabled: boolean) {
   const getCurrentOrders = useCallback(
     async () => await getTypes(searchTerm),
-    [searchTerm]
+    [searchTerm],
   );
   return useQuery({
-    queryKey: ['search', searchTerm],
+    queryKey: ["search", searchTerm],
     queryFn: getCurrentOrders,
     enabled,
   });
